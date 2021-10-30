914998a2f0c9645effa4fe23d7d6407886f69153
estebanzeb <estebanzeb@gmail.com>
2021-10-29 20:29:46 -0500

    FALTA ACOMODAR ALGUNOS ERRORES

--- Momento 2/main.py
+++ Momento 2/main.py
@@ -1,15 +1,14 @@
-from flask import Flask, render_template,url_for,request,redirect
+from flask import Flask, render_template, url_for, request, redirect
 from flask.wrappers import Request
 import user_controller
 
 app = Flask(__name__)
 
-#@app.route('/')
+@app.route('/')
 
-@app.route('/index')
-def index():
-    return render_template('index.html')
-
+# @app.route('/index')
+# def index():
+#     return render_template('index.html')
 
 @app.route('/index_customer')
 def index_customer():
