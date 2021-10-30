d5be7ec6bbe89fe925458127031520bb9a466454
estebanzeb <estebanzeb@gmail.com>
2021-10-29 19:34:19 -0500

    FALTA POR PROBAR EL PROYECTO

--- Momento 2/main.py
+++ Momento 2/main.py
@@ -4,24 +4,26 @@ import user_controller
 
 app = Flask(__name__)
 
-@app.route('/')
+#@app.route('/')
+
 @app.route('/index')
 def index():
+    return render_template('index.html')
+
+
+@app.route('/index_customer')
+def index_customer():
     users = user_controller.get_users()
-    return render_template('index.html',users=users)
+    return render_template('index_customer.html',users=users)
 
 @app.route('/form_add_user')
 def form_add_user():
     return render_template('add_user.html')
 
-@app.route('/form_add_invoice')
-def form_add_invoice():
-    return render_template('add_invoice.html')
-
 @app.route('/edit_user/<int:id>')
 def edit_user(id):
     user = user_controller.get_user_id(id)
-    return render_template('edit_user.html',user=user)
+    return render_template('edit_user.html',user = user)
 
 @app.route('/update_user', methods=['POST'])
 def update_user():
@@ -31,12 +33,12 @@ def update_user():
     status = request.form['status']
     movile = request.form['movile']
     user_controller.update_user(name,status,movile,id)
-    return redirect('/index')
+    return redirect('/index_customer')
 
 @app.route("/delete_user", methods=["POST"])
 def delete_user():
     user_controller.delete_user(request.form["id"])
-    return redirect("/index")
+    return redirect("/index_customer")
 
 @app.route("/save_user", methods=["POST"])
 def save_user():
@@ -45,7 +47,40 @@ def save_user():
     movile = request.form['movile']
     user_controller.add_user(name,status,movile)
     # De cualquier modo, y si todo fue bien, redireccionar
-    return redirect("/index")
+    return redirect("/index_customer")
+
+#----------------------------------------------------------------------------------------------
+
+@app.route('/index_invoice')
+def index_invoice():
+    invoices = user_controller.get_invoice()
+    return render_template('index_invoice.html',invoices=invoices)
+
+@app.route('/form_add_invoice')
+def form_add_invoice():
+    return render_template('add_invoice.html')
+
+@app.route('/edit_invoice/<int:id>')
+def edit_invoice(id):
+    invoice = user_controller.get_invoice_id(id)
+    return render_template('edit_invoice.html',invoice=invoice)
+
+@app.route('/update_invoice', methods=['POST'])
+def update_invoice():
+    # obtener los datos del formulario que invocó este end-point
+    id = request.form['id']
+    number = request.form['number']
+    date = request.form['date']
+    id_users = request.form['id_users']
+    price = request.form['price']
+    balance = request.form['balance']
+    user_controller.update_invoice(number,date,id_users, price, balance,id)
+    return redirect('/index_invoice')
+
+@app.route("/delete_invoice", methods=["POST"])
+def delete_invoice():
+    user_controller.delete_invoice(request.form["id"])
+    return redirect("/index_invoice")
 
 @app.route("/save_invoice", methods=["POST"])
 def save_invoice():
@@ -56,8 +91,7 @@ def save_invoice():
     balance = request.form['id_users']
     user_controller.add_invoice(number,date,id_users, price, balance)
     # De cualquier modo, y si todo fue bien, redireccionar
-    return redirect("/index")
-
+    return redirect("/index_invoice")
 
 if __name__ == "__main__":
-    app.run(port = 4500, debug=True)
+    app.run(port = 8080, debug=True)
--- Momento 2/user_controller.py
+++ Momento 2/user_controller.py
@@ -1,13 +1,6 @@
 # importar el archivo de la conexión a la BD
 from configdb import get_connection
 
-def add_invoice(number,date,id_users, price, balance):
-    cnn = get_connection()
-    with cnn.cursor() as cursor:
-        cursor.execute("INSERT INTO customer (name, status, mobile) VALUES (%s,%s,%s)",(name,status,mobile))
-    cnn.commit()
-    cnn.close()
-
 def add_user(name, status, mobile):
     cnn = get_connection()
     with cnn.cursor() as cursor:
@@ -25,9 +18,21 @@ def update_user(name,status,mobile,id):
 def delete_user(id):
     cnn = get_connection()
     with cnn.cursor() as cursor:
-        cursor.execute("DELETE FROM customer WHERE id = %s",(id))
-    cnn.commit()
-    cnn.close()
+        
+        cursor.execute("SELECT id, date, id_user, price, balance from customer as us INNER JOIN invoice as I on I.id_user= us.id ORDER BY us.id ASC")
+
+        if cursor.rowcount == 0 :
+
+            cursor.execute("DELETE FROM customer WHERE id = %s",(id))
+            cnn.commit()
+            cnn.close() 
+
+        else:
+            print("El cliente todavia tiene facturas")
+
+            #cnn.commit()
+            cnn.close()    
+    
 
 def get_users():
     cnn = get_connection()
@@ -36,6 +41,9 @@ def get_users():
         cursor.execute("SELECT id, name, status, mobile FROM customer")
         customer = cursor.fetchall()
     cnn.close()
+
+    print(cursor.rowcount)
+
     return customer
 
 def get_user_id(id):
@@ -46,3 +54,68 @@ def get_user_id(id):
         customer = cursor.fetchone()
     cnn.close
     return customer
+
+
+#----------------------------------------------------------------------------------------
+
+def add_invoice(number,date,id_users, price, balance):
+    cnn = get_connection()
+    with cnn.cursor() as cursor:
+        cursor.execute("INSERT INTO invoice (number,date,id_users, price, balance) VALUES (%s,%s,%s,%s,%s)",(number,date,id_users, price, balance))
+    cnn.commit()
+    cnn.close()
+
+def update_invoice(number,date,id_users, price, balance,id):
+    cnn = get_connection()
+    with cnn.cursor() as cursor:
+        cursor.execute("UPDATE invoice SET number = %s, date = %s, id_users = %s , price = %s , balance = %s WHERE id = %s",(number,date,id_users, price, balance))
+    cnn.commit()
+    cnn.close()
+
+def delete_invoice(id):
+    cnn = get_connection()
+    with cnn.cursor() as cursor:
+
+        cursor.execute("SELECT balance FROM invoice WHERE id = %s",(id))
+
+        n = cursor.fetchall()
+
+        if n == 0 :
+            cursor.execute("DELETE FROM invoice WHERE id = %s",(id))
+            cnn.commit()
+            cnn.close() 
+
+        else :
+            print ("El total del balance no es cero")
+            #cnn.commit()
+            cnn.close()
+
+def get_invoice():
+    cnn = get_connection()
+    invoice = []
+    with cnn.cursor() as cursor:
+        cursor.execute("SELECT number, date, id_users, price, balance FROM invoice")
+        invoice = cursor.fetchall()
+    cnn.close()
+
+    print(invoice)
+
+    return invoice
+
+def get_invoice_id(id):
+    cnn = get_connection()
+    invoice = None
+    with cnn.cursor() as cursor:
+        cursor.execute("SELECT id, number, date, id_users, price, balance FROM invoice WHERE id = %s",(id))
+        invoice = cursor.fetchone()
+    cnn.close
+    return invoice
+
+def get_(id):
+    cnn = get_connection()
+    invoice = None
+    with cnn.cursor() as cursor:
+        cursor.execute("SELECT id, number, date, id_users, price, balance FROM invoice WHERE id = %s",(id))
+        invoice = cursor.fetchone()
+    cnn.close
+    return invoice
